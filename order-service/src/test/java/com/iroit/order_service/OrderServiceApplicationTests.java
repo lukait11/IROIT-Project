02cffd30f7432594da796a9263f8c2c5dfa710a7
lukait11 <miljanovic.it11.2022@uns.ac.rs>
2026-02-21 19:48:19 +0100

    init

---
+++ order-service/src/test/java/com/iroit/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iroit.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
